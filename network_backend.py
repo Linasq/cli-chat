baefcf2f5842f017e8b56f77298b521be6284f7e
miczie22 <ziebamichal@student.agh.edu.pl>
2025-06-15 21:10:10 +0200

    Modified backend, to handle UDP for server discover, and to use epoll instead of threads

--- network_backend.py
+++ network_backend.py
@@ -1,7 +1,13 @@
 import socket
 import threading
+import select
+import struct
 
 MSG_SIZE = 2048
+MULTICAST_GROUP = '224.0.0.1'
+MULTICAST_PORT = 50000
+_active_clients = {}  # IP -> socket
+
 
 class PersistentClient:
     def __init__(self):
@@ -62,54 +68,102 @@ class PersistentClient:
                 pass
 
 
+def discover_server(timeout=3):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    sock.settimeout(timeout)
+
+    try:
+        sock.sendto(b"DISCOVER_SERVER", (MULTICAST_GROUP, MULTICAST_PORT))
+        data, server = sock.recvfrom(1024)
+        ip, port = data.decode('utf-8').split(':')
+        return ip, int(port)
+    except socket.timeout:
+        return None, None
+    finally:
+        sock.close()
+
+
+
 # ----------------- SERWER ---------------------
 
 
-_active_clients = {}  # IP -> socket
+
+def _start_multicast_responder(tcp_ip, tcp_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MULTICAST_PORT))
+
+    mreq = struct.pack('4sl', socket.inet_aton(MULTICAST_GROUP), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            if data == b"DISCOVER_SERVER":
+                response = f"{tcp_ip}:{tcp_port}"
+                sock.sendto(response.encode('utf-8'), addr)
+        except Exception:
+            continue
+
 
 def init_server(ip, port, handle_client):
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_sock.bind((ip, port))
     server_sock.listen()
+    server_sock.setblocking(False)
 
-    def client_thread(conn, addr):
-        client_ip = addr[0]
-        _active_clients[client_ip] = conn
-        try:
-            while True:
-                data = b''
-                while len(data) < MSG_SIZE:
-                    packet = conn.recv(MSG_SIZE - len(data))
-                    if not packet:
-                        raise ConnectionError("Client disconnected")
-                    data += packet
-                handle_client(client_ip, data)
-        except Exception:
-            pass
-        finally:
-            conn.close()
-            _active_clients.pop(client_ip, None)
+    poller = select.poll()
+    fd_to_sock = {server_sock.fileno(): server_sock}
+    poller.register(server_sock, select.POLLIN)
 
-    def accept_loop():
+    def server_loop():
         while True:
-            conn, addr = server_sock.accept()
-            threading.Thread(target=client_thread, args=(conn, addr), daemon=True).start()
+            events = poller.poll()
+            for fd, event in events:
+                sock = fd_to_sock[fd]
+                if sock is server_sock:
+                    conn, addr = server_sock.accept()
+                    conn.setblocking(False)
+                    fd_to_sock[conn.fileno()] = conn
+                    _active_clients[addr[0]] = conn
+                    poller.register(conn, select.POLLIN)
+                elif event & select.POLLIN:
+                    conn = sock
+                    try:
+                        data = conn.recv(MSG_SIZE)
+                        if data:
+                            client_ip = conn.getpeername()[0]
+                            handle_client(client_ip, data)
+                        else:
+                            raise ConnectionError
+                    except:
+                        poller.unregister(fd)
+                        conn.close()
+                        ip = conn.getpeername()[0]
+                        _active_clients.pop(ip, None)
+                        fd_to_sock.pop(fd, None)
 
-    threading.Thread(target=accept_loop, daemon=True).start()
+    threading.Thread(target=server_loop, daemon=True).start()
+    threading.Thread(target=lambda: _start_multicast_responder(ip, port), daemon=True).start()
+
     return server_sock
 
 
+
 def send_to_client(client_ip, message_bytes):
-    conn = _active_clients.get(client_ip)
-    if not conn:
-        return False
-    try:
-        if len(message_bytes) > MSG_SIZE:
-            message_bytes = message_bytes[:MSG_SIZE]
-        else:
-            message_bytes = message_bytes.ljust(MSG_SIZE, b'\x00')
-        conn.sendall(message_bytes)
-        return True
-    except Exception:
-        _active_clients.pop(client_ip, None)
-        return False
+    for fd, (conn, ip) in _active_clients.items():
+        if ip == client_ip:
+            try:
+                if len(message_bytes) > MSG_SIZE:
+                    message_bytes = message_bytes[:MSG_SIZE]
+                else:
+                    message_bytes = message_bytes.ljust(MSG_SIZE, b'\x00')
+                conn.sendall(message_bytes)
+                return True
+            except Exception:
+                conn.close()
+                _active_clients.pop(fd, None)
+                return False
+    return False
