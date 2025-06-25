9874096888fffc2294b090fbb42998b43428514a
mkraczek <mkraczek1@gmail.com>
2025-06-25 19:16:57 +0200

    key exchange shoud be working

--- crypto_functions.py
+++ crypto_functions.py
@@ -1,13 +1,14 @@
-import json, socket
+import json
 from typing import Any, Dict, Tuple
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidSignature
 
 # --- Helpers ---
 
@@ -26,13 +27,24 @@ def hash_md5(data: bytes) -> str:
     digest.update(data)
     return digest.finalize().hex()
 
-def serialize_public_key(key: X25519PublicKey | Ed25519PublicKey) -> bytes:
-    return key.public_bytes(Encoding.Raw, PublicFormat.Raw)
+def x25519_pubkey_to_bytes(pubkey: X25519PublicKey) -> bytes:
+    return pubkey.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
 
-def deserialize_x25519_public_key(data: bytes) -> X25519PublicKey:
+
+def x25519_pubkey_from_bytes(data: bytes) -> X25519PublicKey:
     return X25519PublicKey.from_public_bytes(data)
 
-def deserialize_ed25519_public_key(data: bytes) -> Ed25519PublicKey:
+
+def ed25519_pubkey_to_bytes(pubkey: Ed25519PublicKey) -> bytes:
+    return pubkey.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
+
+def ed25519_pubkey_from_bytes(data: bytes) -> Ed25519PublicKey:
     return Ed25519PublicKey.from_public_bytes(data)
 
 # --- User key publication ---
@@ -53,15 +65,14 @@ def generate_keys_to_send(user_id: str) -> Tuple[Dict[str, str], Dict[str, Any]]
     PK_pub = PK_priv.public_key()
 
     # SPK = signature of PK_pub using IK_sign_priv
-    pk_pub_bytes = serialize_public_key(PK_pub)
-    signature = IK_sign_priv.sign(pk_pub_bytes)
+    signature = IK_sign_priv.sign(x25519_pubkey_to_bytes(PK_pub))
 
     payload = {
         "type": "publish_keys",
         "user_id": user_id,
-        "IK_sign_pub": IK_sign_pub.public_bytes(Encoding.Raw, PublicFormat.Raw).hex(),
-        "IK_dh_pub": serialize_public_key(IK_dh_pub).hex(),
-        "PK_pub": pk_pub_bytes.hex(),
+        "IK_sign_pub": ed25519_pubkey_to_bytes(IK_sign_pub).hex(),
+        "IK_dh_pub": x25519_pubkey_to_bytes(IK_dh_pub).hex(),
+        "PK_pub": x25519_pubkey_to_bytes(PK_pub).hex(),
         "SPK_sig": signature.hex()
     }
 
@@ -86,10 +97,21 @@ def establish_session_key_initiator(user_id: str, my_keys: Dict[str, Any], recip
     EK_priv = X25519PrivateKey.generate()
     EK_pub = EK_priv.public_key()
 
+    IK_sign_pub = ed25519_pubkey_from_bytes(bytes.fromhex(recipient_keys["IK_sign_pub"]))
+    IK_dh_pub = x25519_pubkey_from_bytes(bytes.fromhex(recipient_keys["IK_dh_pub"]))
+    PK_pub = x25519_pubkey_from_bytes(bytes.fromhex(recipient_keys["PK_pub"]))
+    SPK_sig = bytes.fromhex(recipient_keys["SPK_sig"])
+    pk_pub_bytes = x25519_pubkey_to_bytes(PK_pub)
+
+    try:
+        IK_sign_pub.verify(SPK_sig, pk_pub_bytes)
+    except InvalidSignature:
+        return None, None, None
+
     # DH exchanges
-    DH1 = my_keys["IK_dh_priv"].exchange(recipient_keys["PK_pub"])
-    DH2 = EK_priv.exchange(recipient_keys["IK_dh_pub"])
-    DH3 = EK_priv.exchange(recipient_keys["PK_pub"])
+    DH1 = my_keys["IK_dh_priv"].exchange(PK_pub)
+    DH2 = EK_priv.exchange(IK_dh_pub)
+    DH3 = EK_priv.exchange(PK_pub)
 
     SK = kdf(DH1, DH2, DH3)
     md5_hash = hash_md5(SK)
@@ -98,7 +120,7 @@ def establish_session_key_initiator(user_id: str, my_keys: Dict[str, Any], recip
     ephemerals_to_send = {
             "type": "publish_ephemeral",
             "user_id": user_id,
-            "EK_pub": serialize_public_key(EK_pub).hex()
+            "EK_pub": x25519_pubkey_to_bytes(EK_pub).hex()
             }
 
     return SK, md5_hash, ephemerals_to_send
@@ -106,10 +128,25 @@ def establish_session_key_initiator(user_id: str, my_keys: Dict[str, Any], recip
 # --- Responder (B) ---
 #before calling this function you need to fech for sender keys and ephemearl public key and pass it as arguments for this function
 def establish_session_key_responder(my_keys: Dict[str, Any], sender_keys: Dict[str, Any], ephemeral: Dict[str, Any]):
+    
+    IK_sign_pub = ed25519_pubkey_from_bytes(bytes.fromhex(sender_keys["IK_sign_pub"]))
+    IK_dh_pub = x25519_pubkey_from_bytes(bytes.fromhex(sender_keys["IK_dh_pub"]))
+    PK_pub = x25519_pubkey_from_bytes(bytes.fromhex(sender_keys["PK_pub"]))
+    SPK_sig = bytes.fromhex(sender_keys["SPK_sig"])
+    pk_pub_bytes = x25519_pubkey_to_bytes(PK_pub)
+
+    try:
+        IK_sign_pub.verify(SPK_sig, pk_pub_bytes)
+    except InvalidSignature:
+        return None, None 
+
+    EK_pub = x25519_pubkey_from_bytes(bytes.fromhex(ephemeral["EK_pub"]))
+
     # DH exchanges
-    DH1 = sender_keys["IK_dh_pub"].exchange(my_keys["PK_priv"])
-    DH2 = ephemeral["EK_pub"].exchange(my_keys["IK_dh_priv"])
-    DH3 = ephemeral["EK_pub"].exchange(my_keys["PK_priv"])
+
+    DH1 = my_keys["PK_priv"].exchange(IK_dh_pub)
+    DH2 = my_keys["IK_dh_priv"].exchange(EK_pub)
+    DH3 = my_keys["PK_priv"].exchange(EK_pub)
 
     SK = kdf(DH1, DH2, DH3)
     md5_hash = hash_md5(SK)
